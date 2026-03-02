20283ea86b859957f3269eadac694476ffde9979
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2026-03-02 10:35:02 +0000

    pass negative tests for Rhombus class

--- griffith/Rhombus.java
+++ griffith/Rhombus.java
@@ -15,11 +15,19 @@ public class Rhombus extends Shape {
 
     @Override
     public double perimeter() {
+
+        // no values can be negative, test fails
+        if (sideLength == -1 || diagonalOne == -1 || diagonalTwo == -1) { return -1; }
+
         return 4 * sideLength;
     }
     
     @Override
     public double area() {
+
+        // no values can be negative, test fails
+        if (sideLength == -1 || diagonalOne == -1 || diagonalTwo == -1) { return -1; }
+
         return diagonalOne * diagonalTwo * 0.5;
     }
 
